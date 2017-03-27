4fe4082bf6ee57f854f0c70ac7e8a55b5625a050
gjf <gjf@163.com>
2017-03-27 22:25:29 +0800

    1111

--- itcast-solrj/src/main/java/cn/itcast/solrj/pojo/BasePojo.java
+++ itcast-solrj/src/main/java/cn/itcast/solrj/pojo/BasePojo.java
@@ -2,23 +2,26 @@ package cn.itcast.solrj.pojo;
 
 import java.util.Date;
 
+//addase
 public abstract class BasePojo {
-    
-    private Date created;
-    private Date updated;
-    public Date getCreated() {
-        return created;
-    }
-    public void setCreated(Date created) {
-        this.created = created;
-    }
-    public Date getUpdated() {
-        return updated;
-    }
-    public void setUpdated(Date updated) {
-        this.updated = updated;
-    }
-    
-    
+
+	private Date created;
+	private Date updated;
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+
+	public Date getUpdated() {
+		return updated;
+	}
+
+	public void setUpdated(Date updated) {
+		this.updated = updated;
+	}
 
 }
